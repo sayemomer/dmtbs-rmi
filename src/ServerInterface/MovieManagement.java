670e6cac10dcb5bef8c797092ae900eec2fe80ed
sayemomer <omer_sayem@yahoo.com>
2023-03-07 11:58:59 -0500

    movie model and server interface

---
+++ src/ServerInterface/MovieManagement.java
@@ -0,0 +1,7 @@
+package ServerInterface;
+
+public class MovieManagement {
+    public static final String MOVIE_SERVER_ATWATER = "AWT";
+    public static final String MOVIE_SERVER_VERDUN = "VER";
+    public static final String MOVIE_SERVER_OUTRAMONT = "OUT";
+}
